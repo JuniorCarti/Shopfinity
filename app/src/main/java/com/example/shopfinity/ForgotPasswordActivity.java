2be7aaef29f46d86a94bcebf1e9dbbb360efe38b
JuniorCarti <ridgejunior204@gmail.com>
2025-03-09 15:10:46 +0300

    Code for forgot password activity

--- app/src/main/java/com/example/shopfinity/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/shopfinity/ForgotPasswordActivity.java
@@ -1,24 +1,24 @@
 package com.example.shopfinity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    private TextInputEditText emailInput;
+    private TextView tvMessage;
+    private Button btnSubmit;
+    private FirebaseAuth mAuth;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forgot_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
