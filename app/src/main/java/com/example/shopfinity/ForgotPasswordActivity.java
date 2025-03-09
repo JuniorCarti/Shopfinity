80443ea220a54cea43e22b39ac90102f00845368
JuniorCarti <ridgejunior204@gmail.com>
2025-03-09 15:11:48 +0300

    Initialized Firebase Authentication

--- app/src/main/java/com/example/shopfinity/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/shopfinity/ForgotPasswordActivity.java
@@ -22,3 +22,5 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
