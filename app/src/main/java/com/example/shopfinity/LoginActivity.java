cbc4734c9fbb6d673b5fc95d371f0b21896cbfe0
JuniorCarti <ridgejunior204@gmail.com>
2025-03-09 15:06:49 +0300

    Google Sign In SetUp

--- app/src/main/java/com/example/shopfinity/LoginActivity.java
+++ app/src/main/java/com/example/shopfinity/LoginActivity.java
@@ -48,3 +48,10 @@ public class LoginActivity extends AppCompatActivity {
         // Initialize Firebase
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        // Google Sign-In Setup
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
