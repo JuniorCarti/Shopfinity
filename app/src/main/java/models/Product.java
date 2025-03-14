d3f866889e837609958f8fc69151367b86dd044b
JuniorCarti <ridgejunior204@gmail.com>
2025-03-15 00:37:38 +0300

    Getters and Setters

--- app/src/main/java/models/Product.java
+++ app/src/main/java/models/Product.java
@@ -60,4 +60,62 @@ public class Product implements Serializable {
     public String getSubcategory() { return subcategory; }
     public String getDescription() { return description; }
     public double getPrice() { return price; }
+    @PropertyName("discount_price")
+    public double getDiscountPrice() { return discountPrice; }
 
+    public double getRating() { return rating; }
+    public int getReviews() { return reviews; }
+    public int getStock() { return stock; }
+
+    @PropertyName("image_urls")
+    public List<String> getImageUrls() { return imageUrls; }
+
+    public Map<String, Object> getSpecifications() { return specifications; } // Change to Map
+
+    @PropertyName("best_seller")
+    public boolean isBestSeller() { return bestSeller; }
+
+    public void setId(String id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setBrand(String brand) { this.brand = brand; }
+    public void setCategory(String category) { this.category = category; }
+    public void setSubcategory(String subcategory) { this.subcategory = subcategory; }
+    public void setDescription(String description) { this.description = description; }
+    public void setPrice(double price) { this.price = price; }
+
+    @PropertyName("discount_price")
+    public void setDiscountPrice(double discountPrice) { this.discountPrice = discountPrice; }
+
+    public void setRating(double rating) { this.rating = rating; }
+    public void setReviews(int reviews) { this.reviews = reviews; }
+    public void setStock(int stock) { this.stock = stock; }
+
+    @PropertyName("image_urls")
+    public void setImageUrls(List<String> imageUrls) { this.imageUrls = imageUrls; }
+
+    public void setSpecifications(Map<String, Object> specifications) { this.specifications = specifications; } // Change to Map
+
+    @PropertyName("best_seller")
+    public void setBestSeller(boolean bestSeller) { this.bestSeller = bestSeller; }
+
+    // toString() method for debugging
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", category='" + category + '\'' +
+                ", subcategory='" + subcategory + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", discountPrice=" + discountPrice +
+                ", rating=" + rating +
+                ", reviews=" + reviews +
+                ", stock=" + stock +
+                ", imageUrls=" + imageUrls +
+                ", specifications=" + specifications +
+                ", bestSeller=" + bestSeller +
+                '}';
+    }
+}
