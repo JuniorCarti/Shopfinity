a45cde519edc3df2c7ae9ed6acab14b031133a28
JuniorCarti <ridgejunior204@gmail.com>
2025-03-15 00:41:05 +0300

    Imports for ProductAdapter.java

---
+++ app/src/main/java/adapters/ProductAdapter.java
@@ -0,0 +1,23 @@
+package adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.shopfinity.ProductDetailsActivity;
+import com.example.shopfinity.R;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import models.Product;
