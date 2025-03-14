342439309d34fcca542de8860f910b3a84889fc0
JuniorCarti <ridgejunior204@gmail.com>
2025-03-15 00:32:26 +0300

    Added Strings for the Fields added on CloudFirestore

--- app/src/main/java/models/Product.java
+++ app/src/main/java/models/Product.java
@@ -4,3 +4,12 @@ import com.google.firebase.firestore.PropertyName;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
+
+public class Product implements Serializable {
+    private String id;
+    private String name;
+    private String brand;
+    private String category;
+    private String subcategory;
+    private String description;
+    private double price;
