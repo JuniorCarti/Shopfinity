4395b593a2694d61ece351eef8b9215a2b2fedb5
JuniorCarti <ridgejunior204@gmail.com>
2025-03-19 10:11:42 +0300

    feat: Add WishlistFragment with Firebase integration and RecyclerView setup

--- app/src/main/java/fragments/WishlistFragment.java
+++ app/src/main/java/fragments/WishlistFragment.java
@@ -1,27 +1,30 @@
 package fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.shopfinity.R;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class WishlistFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    public WishlistFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wishlist, container, false);
-    }
-}
+import adapters.WishlistAdapter;
+import models.WishlistItem;
