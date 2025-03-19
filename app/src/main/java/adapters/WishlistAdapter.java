b17c44917fba5f423e0b10bdcfffebce32589f17
JuniorCarti <ridgejunior204@gmail.com>
2025-03-19 09:52:26 +0300

    Initialize WishlistAdapter with context, view binding, and Glide support

---
+++ app/src/main/java/adapters/WishlistAdapter.java
@@ -0,0 +1,15 @@
+package adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.example.shopfinity.R;
+import java.util.List;
+import models.WishlistItem;
